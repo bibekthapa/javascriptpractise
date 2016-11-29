b82cd9dfe8979d4773a36fc1325ee9014eeab709
Bibek <bibek.thapahere@gmail.com>
2016-11-29 08:31:45 -0800

    Added the auto result on entering the num1

---
+++ web/assets/js/calculator.js
@@ -0,0 +1,21 @@
+function Calculator()
+{
+   var obj=this;
+   
+   obj.add=function(x,y)
+   {
+       return x+y;
+       
+   };
+   obj.sub=function(x,y)
+   {
+       
+       return x-y;
+   };
+   
+    
+    return obj;
+    
+}
+
+
